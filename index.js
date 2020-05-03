ed48279f8b59db79e2ffa51a8f90597c89f3a614
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-03 23:58:16 +0300

    m

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ define([], function () {
   return {
     onSave: function () {
       console.log("external on save");
-      console.log(pipelinesArr[0], groupsArr[0]);
+      console.log(pipelinesArr[0]);
+      console.log(groupsArr[0]);
     },
     settings: async (self) => {
       pipelinesArr = [];
