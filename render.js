a12d3bc2ad2f414c576eb7e33322aa36ad1b60f8
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-20 20:47:43 +0300

    m

--- render.js
+++ render.js
@@ -183,14 +183,14 @@ define([], function () {
             });
 
 
-            $(".modal-body").css({ "display": "grid", "grid-template-columns": "1fr 1fr 1fr", "grid-column-gap": "10px", "grid-row-gap": "10px" });
+            // $(".modal-body").css({ "display": "grid", "grid-template-columns": "1fr 1fr 1fr", "grid-column-gap": "10px", "grid-row-gap": "10px" });
 
-            $(".mm_select").css({ "grid-column-start": "1", "grid-column-end": "2" });
-            $(".mm_dataField").css({ "grid-column-start": "2", "grid-column-end": "3" });
-            $(".mm_taskType").css({ "grid-column-start": "3", "grid-column-end": "4" });
+            // $(".mm_select").css({ "grid-column-start": "1", "grid-column-end": "2" });
+            // $(".mm_dataField").css({ "grid-column-start": "2", "grid-column-end": "3" });
+            // $(".mm_taskType").css({ "grid-column-start": "3", "grid-column-end": "4" });
 
-            $(".mm_textaria").css({ "grid-column-start": "1", "grid-column-end": "4" });
-            $(".mm_button").css({ "grid-column-start": "3", "grid-column-end": "4" });
+            // $(".mm_textaria").css({ "grid-column-start": "1", "grid-column-end": "4" });
+            // $(".mm_button").css({ "grid-column-start": "3", "grid-column-end": "4" });
 
 
 
