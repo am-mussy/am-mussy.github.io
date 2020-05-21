2a18feb9702ac9c50ce692140e3aad63ce61275d
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-21 21:11:32 +0300

    m

--- render.js
+++ render.js
@@ -6,7 +6,10 @@ define([], function () {
             thisHttp = document.location.href;
             thisHttpArr = thisHttp.split('/');
 
-            if(thisHttpArr[arr.length-2] === "detail"){
+            if (thisHttpArr[arr.length - 2] === "detail") {
+
+
+                data = `<h1> Hello world </h1>`;
                 modal = new Modal({
                     class_name: 'modal-window',
                     init: function ($modal_body) {
@@ -26,7 +29,7 @@ define([], function () {
 
 
 
-            
+
             // modal = new Modal({
             //     class_name: 'modal-window',
             //     init: function ($modal_body) {
@@ -39,7 +42,7 @@ define([], function () {
             //     },
             //     destroy: function () {
             //     }
-            });
+        });
 
 
 
