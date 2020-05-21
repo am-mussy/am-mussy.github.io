86e9300982c55d184f7e3f731bf9d813aa45fb60
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-21 21:12:34 +0300

    m

--- render.js
+++ render.js
@@ -30,19 +30,19 @@ define([], function () {
 
 
 
-            // modal = new Modal({
-            //     class_name: 'modal-window',
-            //     init: function ($modal_body) {
-            //         var $this = $(this);
-            //         $modal_body
-            //             .trigger('modal:loaded') // запускает отображение модального окна
-            //             .html(data)
-            //             .trigger('modal:centrify')  // настраивает модальное окно
-            //             .append('');
-            //     },
-            //     destroy: function () {
-            //     }
-        });
+            //     modal = new Modal({
+            //         class_name: 'modal-window',
+            //         init: function ($modal_body) {
+            //             var $this = $(this);
+            //             $modal_body
+            //                 .trigger('modal:loaded') // запускает отображение модального окна
+            //                 .html(data)
+            //                 .trigger('modal:centrify')  // настраивает модальное окно
+            //                 .append('');
+            //         },
+            //         destroy: function () {
+            //         }
+            // });
 
 
 
