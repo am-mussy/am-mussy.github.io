c78b2955164f7ad9f846f00813990972dd3eb8cc
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-23 22:34:31 +0300

    m

--- render.js
+++ render.js
@@ -10,23 +10,23 @@ define([], function () {
             const subdomain = "redboxamo";
             const linkUsers = `https://${subdomain}.amocrm.ru/api/v2/account?with=users`;
 
-            async function getUsers(linkUsers) {
-                let response = await fetch(linkUsers);
-                let mm_users = await response.json();
-                mm_users = mm_users._embedded.users;
-                return mm_users;
-            }
+            // async function getUsers(linkUsers) {
+            //     let response = await fetch(linkUsers);
+            //     let mm_users = await response.json();
+            //     mm_users = mm_users._embedded.users;
+            //     return mm_users;
+            // }
 
 
 
-            const mm_users = getUsers(linkUsers);
+            // const mm_users = getUsers(linkUsers);
 
             
-            for (let i of Object.keys(mm_users)) {
-                if (mm_users[i].id === AMOCRM.constant('user').id) {
-                    console.log(mm_users[i].name, mm_users[i].group_id)
-                }
-            }
+            // for (let i of Object.keys(mm_users)) {
+            //     if (mm_users[i].id === AMOCRM.constant('user').id) {
+            //         console.log(mm_users[i].name, mm_users[i].group_id)
+            //     }
+            // }
 
             if (AMOCRM.data.current_entity === "leads") {
 
