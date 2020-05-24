b9bf65f58fb8027adcf21d735ec8ee742feacce9
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-24 13:04:34 +0300

    m

--- render.js
+++ render.js
@@ -27,8 +27,8 @@ define([], function () {
 
             for (let i of Object.keys(mm_users)) {
                 if (mm_users[i].id === AMOCRM.constant('user').id) {
-                    for (let j of self.get_settings().idgroup.checked_groups) {
-                        if (String(mm_users[i].group_id) === j) {
+                    for (let j of Object.keys(self.get_settings().idgroup.checked_groups)) {
+                        if (String(mm_users[i].group_id) === self.get_settings().idgroup.checked_groups[j]) {
                             mm_logick = true;
                             console.log('yes');
                         }
