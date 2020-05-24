a285f3a9eaf80829785e8a6f1c26fe9729de5656
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-24 16:54:34 +0300

    m

--- render.js
+++ render.js
@@ -22,10 +22,10 @@ define([], function () {
             console.log(self.get_settings());
             console.log(typeof self.get_settings().idgroup)
 
-            if (self.get_settings().idgroup != undefined) {
+            if (typeof self.get_settings().idgroup != 'undefined') {
                 for (let i of Object.keys(mm_users)) {
                     if (mm_users[i].id === AMOCRM.constant('user').id) {
-                        if (self.get_settings().idgroup.checked_groups.length > 0 && typeof self.get_settings().idgroup.checked_groups != undefined) {
+                        if (self.get_settings().idgroup.checked_groups.length > 0 && typeof self.get_settings().idgroup.checked_groups != 'undefined') {
                             for (let j of Object.keys(self.get_settings().idgroup.checked_groups)) {
 
                                 if (String(mm_users[i].group_id) === self.get_settings().idgroup.checked_groups[j]) {
