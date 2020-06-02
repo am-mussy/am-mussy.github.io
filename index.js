7e63d2039a12d6547624dc1ab7ac8bec89e952bf
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-06-03 00:21:10 +0300

    m

--- index.js
+++ index.js
@@ -164,10 +164,10 @@ define([], function () {
       $(".mm_userSettings").append("<br>" + mm_button_start + "<br>");
       $(".mm_button_start").css({ "background-color": "rgb(36, 188, 140)", "color": "#005C3B", "float": "right" });
 
-      if (self.get_settings.idgroup.demo === 1) {
-        $(".mm_button_start").css({ "background-color": "rgb(211, 215, 216)", "color": "#005C3B", "float": "right", "border": "1px solid rgb(36, 188, 140)" })
-        $('.mm_button_start').prop('disabled', true);
-      }
+      // if (self.get_settings.idgroup.demo === 1) {
+      //   $(".mm_button_start").css({ "background-color": "rgb(211, 215, 216)", "color": "#005C3B", "float": "right", "border": "1px solid rgb(36, 188, 140)" })
+      //   $('.mm_button_start').prop('disabled', true);
+      // }
 
       //обработчик кнопки "Включить виджет"
       document.getElementById('mm_button_start').addEventListener('click', () => {
