f373f6a8eb94ba9395e3914c8b3237e22939e395
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-06-03 19:27:30 +0300

    m

--- index.js
+++ index.js
@@ -164,6 +164,8 @@ define([], function () {
       $(".mm_userSettings").append("<br>" + mm_button_start + "<br>");
       $(".mm_button_start").css({ "background-color": "rgb(36, 188, 140)", "color": "#005C3B", "float": "right" });
 
+
+      console.log(self.get_settings());
       if (self.get_settings().idgroup != 'undefined') {
         if (self.get_settings().idgroup.demo === 1) {
           $(".mm_button_start").css({ "background-color": "rgb(211, 215, 216)", "color": "#005C3B", "float": "right", "border": "1px solid rgb(36, 188, 140)" })
@@ -186,7 +188,7 @@ define([], function () {
       })
 
 
-      console.log(self.get_settings());
+
 
 
 
