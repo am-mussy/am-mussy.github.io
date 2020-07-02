0def5025499ccf5d778794bc909a0c4bec509c2c
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-07-02 22:22:51 +0300

    m

--- index.js
+++ index.js
@@ -79,12 +79,19 @@ define([], function () {
         old_settings = JSON.parse($("input[name = idgroup]").val());
       }
 
-      dataDB = {
-        subdomain: subdomain,
-        name: 'task',
-        username: old_settings.email,
-        phone: old_settings.phone
+
+      try {
+        dataDB = {
+          subdomain: subdomain,
+          name: 'task',
+          username: old_settings.email,
+          phone: old_settings.phone
+        }
+      } catch (error) {
+        console.log(error)
       }
+
+
 
       //console.log(dataDB)
 
