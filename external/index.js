eaaab493fcbc09c83c48657526a55bd28063ee09
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-07-25 18:34:55 +0300

    m

--- external/index.js
+++ external/index.js
@@ -100,12 +100,13 @@ define([], function () {
               <iframe width="593" height="333" src="https://www.youtube.com/embed/Oap2be7bR0c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
           `);
-          // t
 
           //Вставка селектора групп
           const groups = await getGroups();
+          console.log({ groups });
           const groups_arr = [];
           for (let i of Object.keys(groups)) {
+            console.log({ i });
             groups_arr.push({
               option: groups[i].name,
               name: groups[i].name,
