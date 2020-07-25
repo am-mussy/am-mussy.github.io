00d5bce31ca49bb9d2af7a9dec0d75bca4507de3
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-07-25 17:55:37 +0300

    m

--- external/index.js
+++ external/index.js
@@ -185,45 +185,45 @@ define([], function () {
         }
       );
       $(".userdata").append("<br>" + data + "<br>");
-      //Получаем группы и записываем их в массив, что бы потом сформировать список в настройках
+      Получаем группы и записываем их в массив, что бы потом сформировать список в настройках
 
-      // const groups = await getGroups(linkGroups);
-      // const groups_arr = [];
-      // for (let i of Object.keys(groups)) {
-      //   groups_arr.push({
-      //     option: groups[i].name,
-      //     name: groups[i].name,
-      //     is_checked: () => {
-      //       try {
-      //         return old_settings.checked_groups.includes(String(groups[i].id));
-      //       } catch (error) {
-      //         return false;
-      //       }
-      //     },
-      //     id: groups[i].id,
-      //     prefix: `groupschkbx${groups[i].id}`,
-      //   });
-      // }
+      const groups = await getGroups(linkGroups);
+      const groups_arr = [];
+      for (let i of Object.keys(groups)) {
+        groups_arr.push({
+          option: groups[i].name,
+          name: groups[i].name,
+          is_checked: () => {
+            try {
+              return old_settings.checked_groups.includes(String(groups[i].id));
+            } catch (error) {
+              return false;
+            }
+          },
+          id: groups[i].id,
+          prefix: `groupschkbx${groups[i].id}`,
+        });
+      }
 
-      // //Список групп
-      // var data = self.render(
-      //   { ref: "/tmpl/controls/checkboxes_dropdown.twig" },
-      //   {
-      //     items: groups_arr,
-      //   }
-      // );
-      // $(".mm_piplineSettings").append("<br>" + data + "<br>");
-      // //Обновление данных при изменении настроек
-      // $(".mm_mainSettings").change(function () {
-      //   mm_settings.checked_groups = [];
-      //   $('[ID *= "cbx_drop_groupschkbx"]').each(function (index) {
-      //     if ($(this).parent().parent().hasClass("is-checked")) {
-      //       mm_settings.checked_groups.push($(this).attr("value"));
-      //     }
-      //   });
-      //   $("input[name = idgroup]").val(JSON.stringify(mm_settings));
-      //   console.log($("input[name = idgroup]").val());
-      // });
+      //Список групп
+      var data = self.render(
+        { ref: "/tmpl/controls/checkboxes_dropdown.twig" },
+        {
+          items: groups_arr,
+        }
+      );
+      $(".mm_piplineSettings").append("<br>" + data + "<br>");
+      //Обновление данных при изменении настроек
+      $(".mm_mainSettings").change(function () {
+        mm_settings.checked_groups = [];
+        $('[ID *= "cbx_drop_groupschkbx"]').each(function (index) {
+          if ($(this).parent().parent().hasClass("is-checked")) {
+            mm_settings.checked_groups.push($(this).attr("value"));
+          }
+        });
+        $("input[name = idgroup]").val(JSON.stringify(mm_settings));
+        console.log($("input[name = idgroup]").val());
+      });
     },
   };
 });
